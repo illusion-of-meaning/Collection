9a8ce823bf3a4a68469890b778578e7ea8f2aae8
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-11-12 21:34:43 +0900

    hw complite

--- src/com/asherbakov/hw2/Main.java
+++ src/com/asherbakov/hw2/Main.java
@@ -3,7 +3,7 @@ package com.asherbakov.hw2;
 public class Main {
     public static void main(String[] args) {
         // ДЗ 2 - 2
-//        new Priority();
+        new Priority();
 
         // ДЗ 2 - 3
         NestedList nestedList = new NestedList();
